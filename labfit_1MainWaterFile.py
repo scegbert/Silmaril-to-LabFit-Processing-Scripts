2db1c841ab5edab7d3a448e88f7874de97ac7920
scott <scottegbert219@gmail.com>
2023-04-14 15:34:44 -0600

    updates to explore line strength for large features
    don't impact main portion of code - changes isolated to the bottom

--- labfit_1MainWaterFile.py
+++ labfit_1MainWaterFile.py
@@ -115,8 +115,8 @@ elif d_type == 'air': props_which = ['nu','sw','gamma_air','n_air','sd_self','de
 
 cutoff_s296 = 1E-24 
 
-bin_name = 'B12' # name of working bin (for these calculations)
-d_labfit_kernal = d_labfit_main # d_labfit_main # d_labfit_kp # d_labfit_kp2
+bin_name = 'B40' # name of working bin (for these calculations)
+d_labfit_kernal = d_labfit_kp2 # d_labfit_main # d_labfit_kp # d_labfit_kp2
 
 
 
@@ -954,12 +954,41 @@ unique_arr = locations[unique_idx]  # get the unique elements
 diff2 = np.diff(unique_arr)
 
 
+#%% run files and slowly remove high pressure measurements with saturated features
+
+iter_labfit = 3
+
+print('     labfit iteration #1')
+feature_error = lab.run_labfit(d_labfit_kernal, bin_name, time_limit=90) # need to run one time to send INP info -> REI
+
+i = 1 # start at 1 because we already ran things once
+while feature_error is None and i < iter_labfit: # run X times
+    i += 1
+    print('     labfit iteration #' + str(i)) # +1 for starting at 0, +1 again for already having run it using the INP (to lock in floats)
+    feature_error = lab.run_labfit(d_labfit_kernal, bin_name, use_rei=True, time_limit=90) 
+
+[T, P, wvn, trans, res, wvn_range, cheby, zero_offset] = lab.labfit_to_spectra(d_labfit_kernal, bins, bin_name) # <-------------------
+df_calcs = lab.information_df(d_labfit_kernal, bin_name, bins, cutoff_s296, T, d_old=None) # <-------------------
 
 
+# df_calcs_all = df_calcs[df_calcs.uc_sw>0].copy()
+# df_calcs_no16 = df_calcs[df_calcs.uc_sw>0].copy()
+# df_calcs_no16_8 = df_calcs[df_calcs.uc_sw>0].copy()
+# df_calcs_no16_8_4 = df_calcs[df_calcs.uc_sw>0].copy()
+# df_calcs_no16_8_4_16500 = df_calcs[df_calcs.uc_sw>0].copy()
+# df_calcs_no16_8_4_16500_8500 = df_calcs[df_calcs.uc_sw>0].copy()
+# df_calcs_no16_8_4_3_16500_8500 = df_calcs[df_calcs.uc_sw>0].copy()
+df_calcs_no16_8_4_3_2_16500_8500_1 = df_calcs[df_calcs.uc_sw>0].copy()
 
 
-
-
+# a_df_calcs_all = df_calcs_all[df_calcs_all.ratio_300 > 1]
+# a_df_calcs_no16 = df_calcs_no16[df_calcs_all.ratio_300 > 1]
+# a_df_calcs_no16_8 = df_calcs_no16_8[df_calcs_all.ratio_300 > 1]
+# a_df_calcs_no16_8_4 = df_calcs_no16_8_4[df_calcs_all.ratio_300 > 1]
+# a_df_calcs_no16_8_4_16500 = df_calcs_no16_8_4_16500[df_calcs_all.ratio_300 > 1]
+# a_df_calcs_no16_8_4_16500_8500 = df_calcs_no16_8_4_16500_8500[df_calcs_all.ratio_300 > 1]
+# a_df_calcs_no16_8_4_3_16500_8500 = df_calcs_no16_8_4_3_16500_8500[df_calcs_all.ratio_300 > 1]
+a_df_calcs_no16_8_4_3_2_16500_8500_1 = df_calcs_no16_8_4_3_2_16500_8500_1[df_calcs_all.ratio_300 > 1]
 
 
 
