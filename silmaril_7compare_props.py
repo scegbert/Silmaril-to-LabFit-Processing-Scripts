1348302d9ad14e2432fe6276d1019967de24ee0b
Scott Egbert <scottegbert219@gmail.com>
2023-08-22 10:45:47 -0600

    file for comparing results from different databases

---
+++ silmaril_7compare_props.py
@@ -0,0 +1,86 @@
+
+
+import numpy as np
+import pickle 
+import matplotlib.pyplot as plt
+
+import os
+from sys import path
+path.append(os.path.abspath('..')+'\\modules')
+
+
+import clipboard_and_style_sheet
+clipboard_and_style_sheet.style_sheet()
+
+
+
+
+
+d_meas = r'C:\Users\scott\Downloads'
+
+
+
+
+
+
+
+d_load = os.path.join(d_meas, 'output fit results.pckl')
+
+f = open(d_load, 'rb')
+[T_all, P_all, y_h2o, pathlength, output2020, output2016, outputPaul, outputSceg] = pickle.load(f)
+f.close()
+    
+
+output_all = [output2020, output2016, outputPaul, outputSceg]
+output_names = ['output2020', 'output2016', 'outputPaul', 'outputSceg']
+
+#%%
+
+plt.figure()
+
+
+for i, output in enumerate(output_all): 
+    
+    # plt.plot(T_all, output[:,4], 'x', label=output_names[i])
+
+    plt.plot(T_all, 100*(T_all - output[:,4]) / T_all, 'x', label=output_names[i])
+
+    print('{}     {}     {}'.format(output_names[i], np.mean(100*(T_all - output[:,4]) / T_all), np.std(100*(T_all - output[:,4]) / T_all)))
+
+
+plt.legend()
+
+
+#%%
+
+plt.figure()
+
+for i, output in enumerate(output_all): 
+    
+    # plt.plot(P_all, output[:,4], 'x', label=output_names[i])
+
+    plt.plot(P_all, 100*(P_all - output[:,2]) / P_all, 'x', label=output_names[i])
+
+    print('{}     {}     {}'.format(output_names[i], np.mean(100*(P_all - output[:,2]) / P_all), np.std(100*(P_all - output[:,2]) / P_all)))
+
+plt.legend()
+
+
+#%%
+
+plt.figure()
+
+for i, output in enumerate(output_all): 
+    
+    plt.plot(T_all, output[:,6], 'x', label=output_names[i])
+
+plt.legend()
+
+
+
+
+
+
+
+
+
